661feb6b5de633384764793a3957285221cc631b
FairyPenguin <autorun81@gmail.com>
2024-02-01 20:20:10 +0200

    useWindowDimenstions for Responsive

--- components/DynamicUI/Dynamic.js
+++ components/DynamicUI/Dynamic.js
@@ -1,32 +1,65 @@
-import { View, Text, StyleSheet } from "react-native"
-
+import { View, Text, StyleSheet, useWindowDimensions } from "react-native"
 
 export default function Dynamic() {
+
+
+    const windowWidth = useWindowDimensions().width
+    const windowHeight = useWindowDimensions().height
+
 
     return (
 
         <View style={styles.flexContainer}>
-            <Text style={styles.title}> Dynamic Component </Text>
+            <View
+                style={[styles.box,
+                {
+                    width: windowWidth > 500 ? "70%" : "90%",
+                    height: windowHeight > 600 ? "60%" : "90%",
+                }]}>
+                <Text
+                    style={[styles.title,
+                    { fontSize: windowWidth > 500 ? 50 : 24, }]}>
+                    Dynamic Component
+                </Text>
+            </View>
 
-        </View>
+        </View >
 
     )
-
-
 
 
 }
 
 
+/*
+ Responsive Styles
+
+ The recommended approach is the useWindowDimensions 
+*/
+
+// const windowWidth = Dimensions.get("window")
+// const windowHeight = Dimensions.get("window")
+
+
+// console.log(windowHeight, windowWidth)
+
+
 const styles = StyleSheet.create({
     flexContainer: {
         flex: 1,
-        backgroundColor: "midnightblue",
+        backgroundColor: "white",
+
         alignItems: "center",
         justifyContent: "center"
     },
     title: {
-        fontSize: 33,
-        color: "white"
+
+        color: "black"
+    },
+    box: {
+
+        backgroundColor: "grey",
+        justifyContent: "center",
+        alignItems: "center"
     }
 })
