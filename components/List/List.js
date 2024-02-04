7703137fe38afc162b5d50ebc4e54f871e9e5cea
FairyPenguin <autorun81@gmail.com>
2024-02-04 13:11:32 +0200

    Icon Image

---
+++ components/List/List.js
@@ -0,0 +1,26 @@
+import { View, Text, StyleSheet } from "react-native"
+
+
+export default function NewComponent() {
+
+    return (
+
+        <View>
+            <Text> React Native </Text>
+            <Text> Functional Component </Text>
+
+        </View>
+
+    )
+}
+
+
+const styles = StyleSheet.create({
+    flexContainer: {
+        flex: 0.6,
+        backgroundColor: "white"
+    },
+    title: {
+        fontSize: 33
+    }
+})
